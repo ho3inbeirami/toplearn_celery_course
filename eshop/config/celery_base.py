c1c1cf1f3b1eaf712106a01b016a7588cf6f8840
hosseinbeirami <hosseinbeirami1400@gmail.com>
2026-09-26 08:49:49 +0330

    test priority of tasks

--- eshop/config/celery_base.py
+++ eshop/config/celery_base.py
@@ -7,10 +7,21 @@ app = Celery('config')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-app.conf.task_routs = {
-    'notifications.tasks.send_sms':{'queue':'queue1'},
-    'notifications.tasks.send_email':{'queue':'queue2'}
+# app.conf.task_routs = {
+#     'notifications.tasks.send_sms':{'queue':'queue1'},
+#     'notifications.tasks.send_email':{'queue':'queue2'}
+# }
+
+app.conf.broker_transport_options = {
+    'queue_order_strategy': 'priority',
 }
+
+app.conf.broker_transport_options = {
+    'priority_steps': list(range(10)),
+    'sep': ':',
+    'queue_order_strategy': 'priority',
+}
+
 
 
 app.autodiscover_tasks()
--- eshop/notifications/tasks.py
+++ eshop/notifications/tasks.py
@@ -1,11 +1,43 @@
 from celery import shared_task
+from time import sleep
+
+import logging
 
 
-@shared_task()
-def send_sms():
-    pass
+logger = logging.getLogger(__name__)
+
+@shared_task(queue='celery')
+def task_1():
+    logger.info('task 1 started')
+    sleep(3)
+    logger.info('task 1 end')
+    return
+
+@shared_task(queue='celery:1')
+def task_2():
+    logger.info('task 2 started')
+    sleep(3)
+    logger.info('task 2 end')
+    return
+
+@shared_task(queue='celery:2')
+def task_3():
+    logger.info('task 3 started')
+    sleep(3)
+    logger.info('task 3 end')
+    return
+
+@shared_task(queue='celery:3')
+def task_4():
+    logger.info('task 4 started')
+    sleep(3)
+    logger.info('task 4 end')
+    return
 
 
-@shared_task()
-def send_email():
-    pass
+task_1.delay()
+task_1.delay()
+task_2.delay()
+task_3.delay()
+task_4.delay()
+task_4.delay()
