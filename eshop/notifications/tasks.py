5db43bff587c0630b0316266497b1fcd8b437659
hosseinbeirami <hosseinbeirami1400@gmail.com>
2026-09-26 09:21:08 +0330

    test for group and chain task

--- eshop/notifications/tasks.py
+++ eshop/notifications/tasks.py
@@ -1,43 +1,73 @@
 from celery import shared_task
 from time import sleep
-
 import logging
-
-
 logger = logging.getLogger(__name__)
 
-@shared_task(queue='celery')
+
+
+# @shared_task(queue='celery')
+# def task_1():
+#     logger.info('task 1 started')
+#     sleep(3)
+#     logger.info('task 1 end')
+#     return
+
+# @shared_task(queue='celery:1')
+# def task_2():
+#     logger.info('task 2 started')
+#     sleep(3)
+#     logger.info('task 2 end')
+#     return
+
+# @shared_task(queue='celery:2')
+# def task_3():
+#     logger.info('task 3 started')
+#     sleep(3)
+#     logger.info('task 3 end')
+#     return
+
+# @shared_task(queue='celery:3')
+# def task_4():
+#     logger.info('task 4 started')
+#     sleep(3)
+#     logger.info('task 4 end')
+#     return
+
+from celery import group , chain
+
+@shared_task()
 def task_1():
-    logger.info('task 1 started')
+    
     sleep(3)
-    logger.info('task 1 end')
-    return
 
-@shared_task(queue='celery:1')
-def task_2():
-    logger.info('task 2 started')
+    return 0
+
+
+
+@shared_task()
+def task_2(i):
+
     sleep(3)
-    logger.info('task 2 end')
-    return
 
-@shared_task(queue='celery:2')
-def task_3():
-    logger.info('task 3 started')
+    return i 
+
+
+
+@shared_task()
+def task_3(i):
+
     sleep(3)
-    logger.info('task 3 end')
-    return
 
-@shared_task(queue='celery:3')
-def task_4():
-    logger.info('task 4 started')
+    return i
+
+
+@shared_task()
+def task_4(i):
+
     sleep(3)
-    logger.info('task 4 end')
-    return
+
+    return i
 
 
-task_1.delay()
-task_1.delay()
-task_2.delay()
-task_3.delay()
-task_4.delay()
-task_4.delay()
+# task_group = group(task_1.s(),task_2.s(), task_3.s(),task_4.s())
+# task_chain = chain(task_1.s(),task_2.s(), task_3.s(),task_4.s())
